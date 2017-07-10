2b6030cf05f2d4b32997b2b4c6b52f2f7085a943
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-07-10 11:44:31 -0700

    import intro example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-console.log('App working: open app/index.js to start exploring ES6');
+
+import * as math from './math';
+import $ from 'jquery';
+
+const {num1, num2, zero} = math;
+
+console.log(`The sum of ${num1} and ${num2} is ${math.add(num1, num2)}`);
+console.log(`Divide by zero result: ${math.divide(num1, zero)}`);
+console.log(`The subtraction of ${num1} and ${num2} is ${math.subtract(num1, num2)}`);
+console.log(`The multiplication of ${num1} and ${num2} is ${math.multiply(num1, num2)}`);
+console.log(`The division of ${num1} and ${num2} is ${math.divide(num1, num2)}`);
+
+$('#root').append('<h1>I am helping</h1>');
